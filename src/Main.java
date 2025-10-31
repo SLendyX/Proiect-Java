c9929c26b5580fcceb2bc047d353ab5b79613b0e
SLendyX <slendyx2002@gmail.com>
2025-10-31 23:26:51 +0200

    Create un JFrame basic, 400x400

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     //TODO Creare fereastra joc
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        JFrame frame = new JFrame("Chess");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        frame.setResizable(true);
+        frame.setVisible(true);
     }
 }
