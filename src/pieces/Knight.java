0df78614942cfdd061f713bda4b8a7bafa1cf68b
Andreid2511 <boata.andrei25@gmail.com>
2025-11-08 22:12:24 +0200

    Realizat logica de mutare a calului(fara capturare)

--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -2,6 +2,9 @@ package pieces;
 
 import engine.PiecePosition;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Knight extends Piece {
     public Knight(boolean isWhite) {
         super(isWhite, "knight");
@@ -10,12 +13,29 @@ public class Knight extends Piece {
     //TODO: de creat logica pentru miscare
     @Override
     public boolean canMove(int x, int y, Piece[][] piecesArray){
-        return true;
+        if(x>=0 && x<8  && y>=0 && y<8){
+            return piecesArray[x][y] == null;
+        }
+        return false;
     }
 
     @Override
     public PiecePosition[] getMoves(int x, int y, Piece[][] piecesArray){
+        List<PiecePosition> moves = new ArrayList<>();
+        int[][] offsets = {
+                {2,1},{2,-1},{-2,1},{-2,-1},
+                {1,-2},{-1,-2},{-1,2},{1,2}
+        };
+        for (int[] offset : offsets) {
+            int newX = x + offset[0];
+            int newY = y + offset[1];
 
-        return null;
+            if (canMove(newX, newY, piecesArray)) {
+                //adaugam pozitia disponibila in lista
+                moves.add(new PiecePosition(newX, newY));
+            }
+        }
+
+        return moves.toArray(new PiecePosition[0]);
     }
 }
